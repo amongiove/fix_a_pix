8f883ed26fa74deb8dd2d5365fea5786032f8d36
amongiove <amandamongiove@gmail.com>
2021-07-12 15:00:09 -0500

    fetch request to pictures index

--- fix_a_pix_frontend/src/index.js
+++ fix_a_pix_frontend/src/index.js
@@ -7,12 +7,9 @@ document.addEventListener('DOMContentLoaded', () => {
 function getPictures(){
     fetch(endPoint)
     .then(response => response.json())
-    // .then(json => console.log(json))
 
     .then(pictures => {
-        // remember our JSON data is a bit nested due to our serializer
         pictures.data.forEach(picture => {
-          // double check how your data is nested in the console so you can successfully access the attributes of each individual object
           const pictureMarkup = `
             <div data-id=${picture.id}>
               <img src=${picture.attributes.picture_url} height="200" width="250">
