185c24be5e642ac103ef51afbede33c7dea717b0
amongiove <amandamongiove@gmail.com>
2021-07-13 00:14:00 -0500

    generate picture search method, API call, and fetch request

--- fix_a_pix_backend/app/controllers/api/v1/pictures_controller.rb
+++ fix_a_pix_backend/app/controllers/api/v1/pictures_controller.rb
@@ -1,3 +1,5 @@
+require 'pexels'
+
 class Api::V1::PicturesController < ApplicationController
 
     def index
@@ -6,6 +8,21 @@ class Api::V1::PicturesController < ApplicationController
         #     include: [:puzzles, :category] 
         # } -- would pass in options below if using this
         render json: PictureSerializer.new(pictures)
+    end
+
+    def search
+        keyword = params[:keyword]
+        client = Pexels::Client.new('563492ad6f91700001000001a28dec988d85416a9cf80775977cb110')
+        #TODO hide auth code in .env file
+        #TODO make so dont have to create new client each time
+        photos = client.photos.search(keyword, per_page: 5)
+        puts photos
+        # pexelPics = []
+        # photos.each do |photo|
+        #     pexelPics << photo.url
+        # end
+        #TODO move this functionality into different model
+        render json: photos
     end
 
     def create
--- fix_a_pix_frontend/src/index.js
+++ fix_a_pix_frontend/src/index.js
@@ -26,11 +26,29 @@ function getPictures(){
   
             document.querySelector('#picture-container').innerHTML += pictureMarkup
         })
-      })
+    })
 }
 
 function searchPictureHandler(e) {
     e.preventDefault()
     const keywordInput = document.querySelector("#search-keyword").value
-    console.log(keywordInput);
+    console.log(keywordInput)
+    const search = `http://localhost:3000/api/v1/pictures/search/${keywordInput}`
+    fetch(search)
+    .then(response => response.json())
+
+    .then(photos => {
+        console.log(photos)
+        photos.forEach(photo => {
+          const results = `
+            <div>
+              <img src=${photo.src.original} height="200" width="250">
+            </div>
+            <br><br>`;
+  
+            document.querySelector('#search-pics').innerHTML += results
+
+            //TODO: reset after each new search
+        })
+    })
 }
